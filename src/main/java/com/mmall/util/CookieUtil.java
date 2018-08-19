0e6706fa38e395268b9c3b2eefd49d3cda16784b
isaac <isaacqc@outlook.com>
2018-08-19 10:46:36 +0800

    fix domain

--- src/main/java/com/mmall/util/CookieUtil.java
+++ src/main/java/com/mmall/util/CookieUtil.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 @Slf4j
 public class CookieUtil {
 
-    private final static String COOKIE_DOMAIN = "isaac.com";
+    private final static String COOKIE_DOMAIN = ".isaac.com";
 
     private final static String COOKIE_NAME = "mmall_login_token";
 
